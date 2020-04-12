097476bad1a4d527126230afc4dca1773d596b8e
Mauricio Cornejo Rossi <maurokornejo@gmail.com>
2020-04-13 00:32:26 +0200

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-app';
+  nombre: string = 'CoronaVirus';
+  nombre2:string = 'maurRIciO CoRnEjO rOssI'
+
+  arreglo  = [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI
+
+  porcentaje: number = 0.2020;
+
+  salario : number = 1555.7;
+
+  fecha : Date = new Date();
+
+  activar : boolean = true;
+
+  idioma : string = 'en';
+
+  videoUrl : string = 'https://www.youtube.com/embed/gdKWeboPxgY';
+
+  valorPromise = new Promise<string>((resolve) => {
+
+    setTimeout(()=>{
+      resolve('llegaron los datos');
+    },5000);
+  })
+
+  heroe ={
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 22
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localfr from '@angular/common/locales/fr';
+registerLocaleData(localEs);
+registerLocaleData(localfr);
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'es'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
